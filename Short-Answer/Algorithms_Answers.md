d4c4ada7283b8460f5ed9740dbf6faeb6f945aaf
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-08-16 20:10:34 -0700

    Questions + recursive count

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,17 @@
 
 ## Exercise I
 
-a)
+a) Linear, O(n). This is just a single while loop, with no nested loops. All the additional N's inside the while loop condition don't have a legible impact on time/space complexity. So the input size proportionally increases the speed/slowness of the algorithm.
 
+b) Nested loops! Quadratic time: O(n^2). This is inception. The speed of the algorithm, slows down dramatically based on input size.
 
-b)
-
-
-c)
+c) This is a constant, 0(1). The size of the input has no affect on space or speed. 
 
 ## Exercise II
 
+def egg_dropper(n):
+    # n = number of floors
+    # f = breaking point floor
+    # iterate through all the floors, starting from the lowest, and drop an egg until it breaks, once it breaks, return that value
+    # Runtime complexity would be Linear, 0(n)
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,37 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
-    
-    pass
+    # split the word into individual letters, using loop
+    # once you get down to a single letter, you can start doing comparison
+    # because h is always preceded by t, you can purely search for t, and if you find it, check the next letter in list
+    # if the next letter is h, increment the number of matches
+    # matches = 0
+    # word_array = list(word)
+    # print(f'{word_array}')
+    # for i in range(len(word_array)):
+    #     letter_t = "t"
+    #     letter_h = "h"
+    #     if word_array[i] == letter_t and word_array[i + 1] == letter_h:
+    #         matches += 1
+    # return matches
+
+    # now let's do recursion
+    # base case is there needs to be at least two characters and they need to be th. If it's less than 2, there are no matches.
+
+    matches = 0
+    if len(word) < 2:
+        # no matches possible with less than 2 characters
+        return matches
+    # else start recursing
+    elif (word[0] + word[1]) == 'th':
+        # if first two letters match, increment matches
+        matches += 1
+        # then return matches along with a recursive call that moves through the word
+        # letter by letter
+        # the equation will come out to 1+1+1+1 type thing
+    return count_th(word[1:]) + matches
+
+
+print(count_th("python"))
+
+print(count_th("thethethethe"))
